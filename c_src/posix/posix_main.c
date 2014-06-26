eae7546e41ea447fcc17dcf54857a60a00a517df
Tom Szilagyi <tomszilagyi@gmail.com>
2014-06-26 17:45:29 +0200

    Implement posix backend's stub functions - basic functionality works

--- c_src/posix/posix_main.c
+++ c_src/posix/posix_main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <poll.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "../erlang_serial.h"
 
@@ -23,14 +28,19 @@ struct serial_channel *serial_open(char *port_name, int bufsz)
 	chan = (struct serial_posix *)malloc(sizeof(struct serial_posix));
 	memset(chan, 0, sizeof(struct serial_posix));
 
-	/* TODO alloc/init priv fields */
-
+        chan->fd = open(port_name, O_RDWR | O_NOCTTY);
+        if (chan->fd < 0) {
+		chan->lastError = errno;
+		free(chan);
+		return NULL;
+	}
 	return (struct serial_channel *)chan;
 }
 
 int serial_configure(struct serial_channel *_chan, struct serial_cfg *cfg)
 {
 	struct serial_posix *chan = (struct serial_posix *)_chan;
+	struct termios tio;
 
 	/* Allocate buffers in the structure, and load the configuration into
 	 * the structure.  This is a basic activity used on all platforms.
@@ -40,7 +50,85 @@ int serial_configure(struct serial_channel *_chan, struct serial_cfg *cfg)
 		return -1;
 	}
 
-	/* TODO init device */
+	/* configure serial device */
+	bzero(&tio, sizeof(tio));
+
+        tio.c_cc[VTIME] = 0;
+        tio.c_cc[VMIN] = 1;
+
+        tio.c_cflag = CLOCAL | CREAD;
+
+	switch (cfg->baud_rate) {
+	case 50: tio.c_cflag |= B50; break;
+	case 75: tio.c_cflag |= B75; break;
+	case 110: tio.c_cflag |= B110; break;
+	case 134: tio.c_cflag |= B134; break;
+	case 150: tio.c_cflag |= B150; break;
+	case 200: tio.c_cflag |= B200; break;
+	case 300: tio.c_cflag |= B300; break;
+	case 600: tio.c_cflag |= B600; break;
+	case 1200: tio.c_cflag |= B1200; break;
+	case 2400: tio.c_cflag |= B2400; break;
+	case 4800: tio.c_cflag |= B4800; break;
+	case 9600: tio.c_cflag |= B9600; break;
+	case 19200: tio.c_cflag |= B19200; break;
+	case 38400: tio.c_cflag |= B38400; break;
+	case 57600: tio.c_cflag |= B57600; break;
+	case 115200: tio.c_cflag |= B115200; break;
+	case 230400: tio.c_cflag |= B230400; break;
+	case 460800: tio.c_cflag |= B460800; break;
+	case 500000: tio.c_cflag |= B500000; break;
+	case 576000: tio.c_cflag |= B576000; break;
+	case 921600: tio.c_cflag |= B921600; break;
+	case 1000000: tio.c_cflag |= B1000000; break;
+	case 1152000: tio.c_cflag |= B1152000; break;
+	case 1500000: tio.c_cflag |= B1500000; break;
+	case 2000000: tio.c_cflag |= B2000000; break;
+	case 2500000: tio.c_cflag |= B2500000; break;
+	case 3000000: tio.c_cflag |= B3000000; break;
+	case 3500000: tio.c_cflag |= B3500000; break;
+	case 4000000: tio.c_cflag |= B4000000; break;
+	default: chan->lastError = EINVAL; return -1;
+	}
+
+	switch (cfg->byte_size) {
+	case 5: tio.c_cflag |= CS5; break;
+	case 6: tio.c_cflag |= CS6; break;
+	case 7: tio.c_cflag |= CS7; break;
+	case 8: tio.c_cflag |= CS8; break;
+	default: chan->lastError = EINVAL; return -1;
+	}
+
+	if (cfg->use_xonxoff) {
+		tio.c_iflag |= IXON | IXOFF | IXANY;
+		tio.c_cc[VSTART] = 0x11; // XON / DC1;
+		tio.c_cc[VSTOP] = 0x13; // XOFF / DC3;
+	} else if (cfg->use_rts && cfg->use_cts) {
+		tio.c_cflag |= CRTSCTS;
+	}
+
+	switch (cfg->parity) {
+	case SERIAL_PARITY_NONE:
+		tio.c_iflag |= IGNPAR;
+		break;
+	case SERIAL_PARITY_EVEN:
+		tio.c_cflag |= PARENB;
+		tio.c_iflag |= INPCK | ISTRIP;
+		break;
+	case SERIAL_PARITY_ODD:
+		tio.c_cflag |= PARENB | PARODD;
+		tio.c_iflag |= INPCK | ISTRIP;
+		break;
+	}
+
+	switch (cfg->stop_bits)	{
+	case SERIAL_STOPBITS_1: break;
+	case SERIAL_STOPBITS_1_5: /* TODO: is this possible at all? */ break;
+	case SERIAL_STOPBITS_2: tio.c_cflag |= CSTOPB; break;
+	}
+
+        tcflush(chan->fd, TCIFLUSH);
+        tcsetattr(chan->fd, TCSANOW, &tio);
 
 	return 0;
 }
@@ -56,16 +144,23 @@ int serial_flush(struct serial_channel *_chan)
 int serial_write(struct serial_channel *_chan, void *buf, int len)
 {
 	struct serial_posix *chan = (struct serial_posix *)_chan;
-	/* TODO */
-	return -1;
+	int ret;
+
+	if ((ret = write(chan->fd, buf, len)) < 0) {
+		chan->lastError = errno;
+	}
+	return ret;
 }
 
-int serial_read(struct serial_channel *_chan, void *_buf, int len)
+int serial_read(struct serial_channel *_chan, void *buf, int len)
 {
 	struct serial_posix *chan = (struct serial_posix *)_chan;
-	/* TODO */
-
-	return -1;
+	int ret;
+	
+	if ((ret = read(chan->fd, buf, len)) < 0) {
+		chan->lastError = errno;
+	}
+	return ret;
 }
 
 void serial_close(struct serial_channel *_chan)
@@ -73,9 +168,7 @@ void serial_close(struct serial_channel *_chan)
 	struct serial_posix *chan = (struct serial_posix *)_chan;
 
 	serial_destroy_struct(_chan);
-
-	/* TODO */
-
+	close(chan->fd);
 	free(chan);
 }
 
@@ -114,13 +207,7 @@ void serial_format_error(struct serial_channel *chan, int ecode, char *msgbuf, i
 
 
 struct pipe_posix {
-
-	int fd;
-
-	/* Error of the last call. This is directly from errno */
-	int lastError;
-
-	/* TODO */
+	int lastError; /* Error of the last call. This is directly from errno */
 };
 
 struct pipe_channel *pipe_os_open(int bufsz)
@@ -130,7 +217,9 @@ struct pipe_channel *pipe_os_open(int bufsz)
 	chan = (struct pipe_posix *)malloc(sizeof(struct pipe_posix));
 	memset(chan, 0, sizeof(struct pipe_posix));
 
-	/* Open channels to Erlang. These are stdin and stdout. */
+	/* NB. stdin and stdout are already open and available as the fds
+	 * STDIN_FILENO and STDOUT_FILENO.
+	 */
 
 	return (struct pipe_channel *)chan;
 }
@@ -138,32 +227,46 @@ struct pipe_channel *pipe_os_open(int bufsz)
 int pipe_write(struct pipe_channel *_chan, void *buf, int len)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
+	int ret;
 
-	/* TODO */
-	return -1;
+	if ((ret = write(STDOUT_FILENO, buf, len)) < 0) {
+		chan->lastError = errno;
+	}
+	return ret;
 }
 
 int pipe_write2(struct pipe_channel *_chan,
 		void *buf1, int len1, void *buf2, int len2)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
+	int ret1, ret2;
 
-	/* TODO */
-	return -1;
+	if ((ret1 = write(STDOUT_FILENO, buf1, len1)) < 0) {
+		chan->lastError = errno;
+		return ret1;
+	}
+	if ((ret2 = write(STDOUT_FILENO, buf2, len2)) < 0) {
+		chan->lastError = errno;
+		return ret2;
+	}
+	return ret1 + ret2;
 }
 
 int pipe_read(struct pipe_channel *_chan, void *buf, int len)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
+	int ret;
 
-	/* TODO */
-	return -1;
+	if ((ret = read(STDIN_FILENO, buf, len)) < 0) {
+		chan->lastError = errno;
+	}
+	return ret;
 }
 
 void pipe_close(struct pipe_channel *_chan)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
-	/* TODO */
+	/* No need to explicitly close stdin and stdout. */
 	free(chan);
 }
 
@@ -171,7 +274,7 @@ struct pollfd pipe_os_readable(struct pipe_channel *_chan)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
 	struct pollfd ret;
-	ret.fd = chan->fd;
+	ret.fd = STDIN_FILENO;
 	ret.events = POLLIN;
 	return ret;
 }
@@ -180,7 +283,7 @@ struct pollfd pipe_os_writeable(struct pipe_channel *_chan)
 {
 	struct pipe_posix *chan = (struct pipe_posix *)_chan;
 	struct pollfd ret;
-	ret.fd = chan->fd;
+	ret.fd = STDOUT_FILENO;
 	ret.events = POLLOUT;
 	return ret;
 }
@@ -210,7 +313,6 @@ static void main_loop(struct serial_port *port)
 	char dbgmsg[256];
 #endif
 
-	int erts_fd = ((struct pipe_posix*)(port->erts))->fd;
 	int serial_fd = ((struct serial_posix*)(port->chan))->fd;
 	int i;
 
@@ -255,12 +357,10 @@ static void main_loop(struct serial_port *port)
 		pres = poll(pollfds, nfds, -1);
 
 		for (i = 0; i < nfds; i++) {
-			if (pollfds[i].fd == erts_fd) {
-				if (pollfds[i].revents & POLLIN) {
-					serial_port_ertsr(port);
-				} else if (pollfds[i].revents & POLLOUT) {
-					serial_port_ertsw(port);
-				}
+			if ((pollfds[i].fd == STDIN_FILENO) && (pollfds[i].revents & POLLIN)) {
+				serial_port_ertsr(port);
+			} else if ((pollfds[i].fd == STDOUT_FILENO) && (pollfds[i].revents & POLLOUT)) {
+				serial_port_ertsw(port);
 			} else if (pollfds[i].fd == serial_fd) {
 				if (pollfds[i].revents & POLLIN) {
 					serial_port_serialr(port);
